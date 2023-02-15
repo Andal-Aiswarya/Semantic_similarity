eeb88d9212e42683fcc00766577de3e4c881f00a
Aiswarya <124206478+Andal-Aiswarya@users.noreply.github.com>
2023-02-15 16:51:13 +0000

    Add files via upload

---
+++ semantic.py
@@ -0,0 +1,34 @@
+import spacy
+nlp = spacy.load('en_core_web_md')
+word1 = nlp("cat")
+word2 = nlp("monkey")
+word3 = nlp("banana")
+print(word1.similarity(word2))
+print(word3.similarity(word2))
+print(word3.similarity(word1))
+
+"""
+Similarities to cat and monkey -0.5929930274321619
+Similarities to banana and monkey -0.40415016164997786
+Similarities to banana and cat 0.22358825939615987
+
+Here cat and monkey are animals so got high similarity level and cat and  banana got the low similarity level as it is
+not related much. However, monkey and banana got medium similarity level as monkey like to eat banana very much. 
+"""
+
+tokens = nlp('cat apple monkey banana ')
+for token1 in tokens:
+    for token2 in tokens:
+        print(token1.text, token2.text, token1.similarity(token2))
+
+
+sentence_to_compare = "Why is my cat on the car"
+sentences = ["where did my dog go",
+"Hello, there is my car",
+"I\'ve lost my car in my car",
+"I\'d like my boat back",
+"I will name my dog Diana"]
+model_sentence = nlp(sentence_to_compare)
+for sentence in sentences:
+    similarity = nlp(sentence).similarity(model_sentence)
+    print(sentence + " - ", similarity)
